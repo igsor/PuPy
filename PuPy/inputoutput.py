ef7cf5f4b25378e361ba65a330f2a1ad877aa0be
baumgartner <baumgartner@10eb3eed-6697-4d8c-9aac-32ebf1d36239>
2013-07-12 14:31:51 +0000

    Added clear_empty_groups
    git-svn-id: https://src.ifi.uzh.ch/ailab/esmcs/src/python/lib/PuPy@1404 10eb3eed-6697-4d8c-9aac-32ebf1d36239

--- PuPy/inputoutput.py
+++ PuPy/inputoutput.py
@@ -4,3 +4,12 @@ Code for input/output handling
 
 """
 
+def clear_empty_groups(pth):
+    """Remove HDF5 groups with no datasets from ``pth``."""
+    import h5py
+    f = h5py.File(pth,'a')
+    for k in f.keys():
+        if len(f[k].keys()) == 0:
+            del f[k]
+    
+    f.close()
