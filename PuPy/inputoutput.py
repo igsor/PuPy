3c0022d1b6cb0eb9a9cbb8db76f460c41bf51580
baumgartner <baumgartner@10eb3eed-6697-4d8c-9aac-32ebf1d36239>
2013-07-12 14:31:51 +0000

    minor change
    git-svn-id: https://src.ifi.uzh.ch/ailab/esmcs/src/python/lib/PuPy@1405 10eb3eed-6697-4d8c-9aac-32ebf1d36239

--- PuPy/inputoutput.py
+++ PuPy/inputoutput.py
@@ -5,11 +5,16 @@ Code for input/output handling
 """
 
 def clear_empty_groups(pth):
-    """Remove HDF5 groups with no datasets from ``pth``."""
+    """Remove HDF5 groups with no datasets from ``pth``. Return a list
+    of removed groups."""
     import h5py
     f = h5py.File(pth,'a')
+    deleted = []
     for k in f.keys():
         if len(f[k].keys()) == 0:
+            deleted.append(k)
             del f[k]
     
     f.close()
+    return deleted
+
